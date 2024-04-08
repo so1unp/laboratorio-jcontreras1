c4ef718c7ffa16cb98789556257268be19c84b5c
jony <jonathan.contreras.991@gmail.com>
2024-04-08 15:53:39 -0300

    Comienzo de trabajo con forkprio y señales

--- lab-03/forkprio.c
+++ lab-03/forkprio.c
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 #include <sys/times.h>      // times()
 
+#define MAX_HIJOS 10
+#define MIN_HIJOS 1
+
+#define MAX_SEGS 10
+#define MIN_SEGS 1
+
 int busywork(void)
 {
     struct tms buf;
@@ -12,5 +18,33 @@ int busywork(void)
 
 int main(int argc, char *argv[])
 {
+    if(argc < 4){
+        printf("Uso: forkprio x y z. X: numero de procesos hijos a crear. Y: El segundo parámetro es el número de segundos que deben ejecutar. Z: puede tomar valores 1 o 0 para reducir o no las prioridades. \n");
+        exit(EXIT_SUCCESS);
+    }
+
+    int nHijos = atoi(argv[1]);
+    int segundosEjecucion = atoi(argv[2]);
+    int prioridad = atoi(argv[3]);
+
+    /* Validación de parametros */
+    if(nHijos < MIN_HIJOS || nHijos > MAX_HIJOS){
+        printf("Cantidad de hijos (primer parametro), debe estar entre: %d y %d\n", MIN_HIJOS, MAX_HIJOS);
+        exit(EXIT_SUCCESS);
+    }
+
+    if(segundosEjecucion < MIN_SEGS || segundosEjecucion > MAX_SEGS){
+        printf("Cantidad de segundos de ejecucion (segundo parametro), debe estar entre: %d y %d\n", MIN_SEGS, MAX_SEGS);
+        exit(EXIT_SUCCESS);
+    }
+    if(prioridad != 0 && prioridad != 1){
+        printf("Los valores posibles para el tercer parametro son 1 y 0 para reducir (o no), la prioridad.\n");
+        exit(EXIT_SUCCESS);
+    }
+
+    char *txtReduccionPrioridad = (prioridad == 0) ? "Sin" : "Con"; 
+    printf("Creando %d hijos, con una ejecucion de %d segundos. %s reducción de prioridad.\n", nHijos, segundosEjecucion, txtReduccionPrioridad); 
+
+
     exit(EXIT_SUCCESS);
 }
--- lab-03/sig_receiver.c
+++ lab-03/sig_receiver.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <string.h>
 
-int main(int argc, char *argv[])
+void handleSignal(int signal){
+    char msg[100];
+    snprintf(msg, sizeof(msg), "%d: %s\n", signal, strsignal(signal));
+    write(STDOUT_FILENO, msg, strlen(msg));
+    fflush(stdout);
+}
+
+int main(void)
 {
+    /*Estructura de manejo de señales*/
+    struct sigaction sa;
+    sa.sa_handler = handleSignal;
+
+    //Atrapamos todas las seniales de la 1 a la 31
+    int indiceSenial;
+    for(indiceSenial = 1; indiceSenial < NSIG; indiceSenial++){
+        //signal(indiceSenial, handleSignal);
+        sigaction(indiceSenial, &sa, NULL);
+    }
+
+    while(1){
+        fflush(stdout);
+        pause();        
+    }
+    
     exit(EXIT_SUCCESS);
 }
