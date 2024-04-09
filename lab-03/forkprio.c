acc6a5ecc10be9c605faa032851a8755d9673970
jony <jonathan.contreras.991@gmail.com>
2024-04-09 12:57:55 -0300

    Trabajando con forkprio

--- lab-03/forkprio.c
+++ lab-03/forkprio.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/times.h>      // times()
+#include <unistd.h>
+#include <sys/resource.h>
+#include <sys/wait.h>
 
 #define MAX_HIJOS 10
 #define MIN_HIJOS 1
 
 #define MAX_SEGS 10
-#define MIN_SEGS 1
+#define MIN_SEGS 0
 
 int busywork(void)
 {
@@ -16,10 +19,33 @@ int busywork(void)
     }
 }
 
+// Handler para la señal de alarma
+void alarm_handler(int signal) {
+    switch (signal)
+	{
+	case SIGTERM:
+		printf("%d: Mi trabajo aquí ya está hecho...\n", getpid());
+        exit(EXIT_SUCCESS);
+		break;
+	case SIGUSR1:
+		//Imprimi algo de info
+       	printf("Child %d (nice %2d):\t%3li\n", getpid(), getpriority(PRIO_PROCESS, 0), 10000000000000);
+		break;
+	case SIGUSR2:
+		//Bajate un cacho la prior
+		nice(1);
+		break;
+	default:
+		printf("%d: Mi trabajo aquí ya está hecho...\n", getpid());
+        exit(EXIT_SUCCESS);
+		break;
+	}
+}
+
 int main(int argc, char *argv[])
 {
     if(argc < 4){
-        printf("Uso: forkprio x y z. X: numero de procesos hijos a crear. Y: El segundo parámetro es el número de segundos que deben ejecutar. Z: puede tomar valores 1 o 0 para reducir o no las prioridades. \n");
+        printf("Uso: forkprio <cant_hijos> <segundos_ejecucion_hijos> <reducir_prioridades [0-1]>.\n");
         exit(EXIT_SUCCESS);
     }
 
@@ -41,9 +67,74 @@ int main(int argc, char *argv[])
         printf("Los valores posibles para el tercer parametro son 1 y 0 para reducir (o no), la prioridad.\n");
         exit(EXIT_SUCCESS);
     }
-
+    
     char *txtReduccionPrioridad = (prioridad == 0) ? "Sin" : "Con"; 
     printf("Creando %d hijos, con una ejecucion de %d segundos. %s reducción de prioridad.\n", nHijos, segundosEjecucion, txtReduccionPrioridad); 
+
+    /**Creación de hijos*/
+    pid_t hijos[nHijos];
+    int indiceHijo;
+    int indiceTiempo;
+   // int nuevoHijo;
+    //Para utilizar los usos de tiempo
+    //struct rusage usage;
+    //Para calcular las diferencias de tiempo
+    //struct timeval start, end;
+
+    // Establecer el manejador de señal para SIGALRM
+
+    for(indiceHijo = 0; indiceHijo < nHijos; indiceHijo++){
+        if((hijos[indiceHijo] = fork()) == 0){
+            signal(SIGUSR1, alarm_handler);
+            signal(SIGUSR2, alarm_handler);
+            signal(SIGTERM, alarm_handler);
+            busywork();
+            //getrusage(RUSAGE_SELF, &usage);
+            //start = usage.ru_stime;
+            /*Hijo*/
+            
+               //getrusage(RUSAGE_SELF, &usage);
+               //end = usage.ru_stime;
+               //long elapsed_time = (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1000000.0;
+               //printf("Child %d (nice %2d):\t%3li\n", getpid(), getpriority(PRIO_PROCESS, 0), elapsed_time);
+                
+            exit(EXIT_SUCCESS);
+        }else{
+ 
+             
+        }
+    }
+    
+	int i, j;
+	/**Tiempo determinado o forever*/
+	if(segundosEjecucion == 0){
+		for(;;){
+			sleep(1);
+			for(j = 0; j < nHijos; j++){
+				kill(hijos[j], SIGUSR1);
+				if(prioridad)
+					kill(hijos[j], SIGUSR2);
+			}
+		}
+	}else{
+		for(i = 0; i < segundosEjecucion; i++){
+			sleep(1);
+			for(j = 0; j < nHijos; j++){
+				kill(hijos[j], SIGUSR1);
+				if(prioridad)
+					kill(hijos[j], SIGUSR2);
+			}
+		}
+	}
+
+
+    for (indiceHijo = 0; indiceHijo < nHijos; indiceHijo++) {
+        kill(hijos[indiceHijo], SIGTERM);
+    }
+    for(indiceTiempo = 0; indiceTiempo < segundosEjecucion; indiceTiempo++){
+        wait(NULL);
+    }
+   //printf("Proceso hijo terminó.\n");
 
 
     exit(EXIT_SUCCESS);
