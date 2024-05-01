13e0f33356ded19770267403ada6bdefde7893d7
jony <jonathan.contreras.991@gmail.com>
2024-05-01 11:56:33 -0300

    glob implementation

--- lab-04/glob.c
+++ lab-04/glob.c
@@ -1,25 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <semaphore.h>
 
 // Variable global que incrementan los hilos.
 static long glob = 0;
-
-void increment_glob(int loops)
+sem_t mutex;
+void increment_glob(int *loops)
 {
     int loc, j;
-
     // incrementa glob
-    for (j = 0; j < loops; j++) {
+    for (j = 0; j < *loops; j++) {
+        sem_wait(&mutex);
         loc = glob;
         loc++;
         glob = loc;
+        sem_post(&mutex);
     }
 }
 
 int main(int argc, char *argv[])
 {
-    int loops;
+    int loops, i;
+    int cantidadHilos = 2;
+    pthread_t hilos[cantidadHilos];
 
     // Controla numero de argumentos.
     if (argc != 2) {
@@ -35,7 +39,31 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
-    printf("%ld\n", glob);
+    printf("Valor antes: %ld\n", glob);
+
+    if (sem_init(&mutex, 0, 1) != 0) {
+        perror("sem_init");
+        exit(EXIT_FAILURE);
+    }
+    
+    // Crea los hilos
+    for (i = 0; i < cantidadHilos; i++) {
+        if (pthread_create(&hilos[i], NULL, (void *) increment_glob, &loops) != 0) {
+            perror("phtread_create");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    // Espera la finalizacion de la ejecución
+    for (i = 0; i < cantidadHilos; i++) {
+        if (pthread_join(hilos[i], NULL) != 0) {
+            perror("pthread_join");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    sem_destroy(&mutex);
+    printf("Valor despues: %ld\n", glob);
 
     exit(EXIT_SUCCESS);
 }
