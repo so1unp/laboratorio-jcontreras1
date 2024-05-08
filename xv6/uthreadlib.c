a5635abe92a3a0f625ee527ec311b521e7261803
jony <jonathan.contreras.991@gmail.com>
2024-05-07 23:09:14 -0300

    Lab4 - XV6 threads imlpementation

--- xv6/uthreadlib.c
+++ xv6/uthreadlib.c
@@ -16,7 +16,7 @@ struct thread {
 };
 
 typedef struct thread thread_t, *thread_p;
-
+static int lastIndex = 0;
 static thread_t all_thread[MAX_THREAD];
 
 thread_p  current_thread;
@@ -40,21 +40,30 @@ thread_init(void)
 void 
 thread_schedule(void)
 {
-  thread_p t;
+	thread_p t;
+	int i, currIndex;
+  	/* 
+	Find another runnable thread. 
+	ACA HAY QUE MODIFICAR usar modulo sobre MAX_THREAD
+	Y otro indice externo
+	*/
+  	next_thread = 0;
 
-  /* Find another runnable thread. ACA HAY QUE MODIFICAR*/
-  next_thread = 0;
-  for (t = all_thread; t < all_thread + MAX_THREAD; t++) {
-    if (t->state == RUNNABLE && t != current_thread) {
-      next_thread = t;
-      break;
-    }
-  }
+  	//for (t = all_thread; t < all_thread + MAX_THREAD; t++) { //original
+	for(i = 1; i < MAX_THREAD; i++){
+		currIndex = (lastIndex + i) % MAX_THREAD;
+		t = &all_thread[currIndex];
+    	if (t->state == RUNNABLE && t != current_thread) {
+      		next_thread = t;
+	   		lastIndex = currIndex;
+      		break;
+    	}
+  	}
 
-  if (t >= all_thread + MAX_THREAD && current_thread->state == RUNNABLE) {
-    /* The current thread is the only runnable thread; run it. */
-    next_thread = current_thread;
-  }
+  	if (t >= all_thread + MAX_THREAD && current_thread->state == RUNNABLE) {
+  	  /* The current thread is the only runnable thread; run it. */
+  	  next_thread = current_thread;
+  	}
 
   if (next_thread == 0) {
     printf(2, "thread_schedule: no runnable threads\n");
